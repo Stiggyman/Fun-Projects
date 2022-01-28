4110ab9695273c56b15c6397f943dcfebd06620b
Stig Mogensen <stig0574@gmail.com>
2022-01-28 22:50:19 +0100

    Fun FizzBuzz project. V1 trying to make it OOP

---
+++ FizzBuzz/FizzBuzz/Models/FizzBuzz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public int Fizz = 3;
+        public int Buzz = 5;
+    }
+}
